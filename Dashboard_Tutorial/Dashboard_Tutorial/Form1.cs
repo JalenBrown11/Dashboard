bde6e912f31d53e8743abf7cc56e979ea091a780
Jalen Brown <64171453+JalenBrown11@users.noreply.github.com>
2022-07-27 03:08:53 -0400

    Add Dashboard files

---
+++ Dashboard_Tutorial/Dashboard_Tutorial/Form1.cs
@@ -0,0 +1,107 @@
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Dashboard_Tutorial
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect, 
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnDashboard.Height;
+            pnlNav.Top = btnDashboard.Top;
+            pnlNav.Left = btnDashboard.Left;
+            btnDashboard.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnAnalytics_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnAnalytics.Height;
+            pnlNav.Top = btnAnalytics.Top;
+            btnAnalytics.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnCalender_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnCalender.Height;
+            pnlNav.Top = btnCalender.Top;
+            btnCalender.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnContactUs_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnContactUs.Height;
+            pnlNav.Top = btnContactUs.Top;
+            btnContactUs.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            pnlNav.Height = btnSettings.Height;
+            pnlNav.Top = btnSettings.Top;
+            btnSettings.BackColor = Color.FromArgb(46, 51, 73);
+        }
+
+        private void btnDashboard_Leave(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnAnalytics_Leave(object sender, EventArgs e)
+        {
+            btnAnalytics.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnCalender_Leave(object sender, EventArgs e)
+        {
+            btnCalender.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnContactUs_Leave(object sender, EventArgs e)
+        {
+            btnContactUs.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(24, 30, 54);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.flaticon.com/") {UseShellExecute = true});
+        }
+    }
+}
